5c50468610caa7821f5551f27b64fefb626639c1
Russell Gapasin <russell@fanswifi.com>
2021-10-25 01:20:02 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import ChatRoom from './ChatRoom';
+import SignIn from './SignIn';
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const app = initializeApp({
+  apiKey: "AIzaSyAEBVkCPzvl2-H3ElkVLLUEklgl1f-STm8",
+  authDomain: "chat-app-4c8df.firebaseapp.com",
+  projectId: "chat-app-4c8df",
+  storageBucket: "chat-app-4c8df.appspot.com",
+  messagingSenderId: "220779545878",
+  appId: "1:220779545878:web:fa6a6b2a9e2057857ebc37"
+});
+
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 function App() {
+  const [user] = useAuthState(auth)
+  const props = {
+    auth,
+    db,
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/chat-app-firebase'>
+          {user ? <Redirect to='/chat-app-firebase/chat-room' /> : <SignIn {...props} />}
+        </Route>
+        <Route exact path='/chat-app-firebase/chat-room'>
+          {user ? <ChatRoom {...props} /> : <Redirect to='/chat-app-firebase' />}
+        </Route>
+      </div>
+    </Router>
   );
 }
 
